dde5f6cdf312c8cd8ce0b686f12909f85e40f5aa
MSA <crystalbluetech@gmail.com>
2025-05-24 17:31:35 +0100

    implement article app and templatee mapping to main ur

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
